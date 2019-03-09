fcefdf81b6de65326c7efa7667944bdce1b64f12
Mohsin Javed <mhsnjvd@gmail.com>
2019-03-09 19:36:01 +0000

    add model view and urls for vis!

--- vis/models.py
+++ vis/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from internal_rate_of_return import IRR, Company
+from .internal_rate_of_return.irr import IRR, Company
 
 
 
--- vis/urls.py
+++ vis/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-            path('', views.post_company_names, name='index'),
+            path('<company_ticker>', views.post_company_data, name='company_data_view'),
+            path('', views.post_company_tickers, name='index'),
             ]
+
+        
